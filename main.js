9f064c35f293fb5c5ed141f05447927053ca9758
Andres Cifuentes <aacifuentes@gmail.com>
2017-03-13 22:11:32 -0500

    all fns done

--- main.js
+++ main.js
@@ -3,6 +3,17 @@ var lastName = document.getElementById('lname');
 var submit = document.getElementById('submit');
 var email = document.getElementById('email');
 var country = document.getElementById('country');
+var bio = document.getElementById('bio');
+var phone = document.getElementById('phone');
+var affil = document.getElementById('affil');
+var occupation = document.getElementById('occupation');
+var cat = document.getElementById('cat');
+var talent = document.getElementById('talent');
+var drink = document.getElementById('drink');
+var power = document.getElementById('power');
+var weapon = document.getElementById('weapon');
+var comments = document.getElementById('comments');
+
 var obj = {}
 
 // firstName.value =
@@ -12,6 +23,17 @@ submit.addEventListener('click', function(){
   firstLast();
   emailInc();
   getCountry();
+  biography();
+  getPhone();
+  getOccupation();
+  getCat();
+  favGadget();
+  getDrink();
+  getPower();
+  getWeapon();
+  getTalent();
+  affiliation();
+  comment();
   console.log(obj);
 })
 
@@ -54,11 +76,102 @@ function getCountry(){
   obj.country = country.value;
 }
 
+function biography(){
+  if(bio.value.length < 20){
+    alert("biography is too short")
+  } else {
+    obj.biography = bio.value;
+  }
+};
+
+function getPhone (){
+  if (phone.value.length < 13){
+    alert("please use (561)777-7777 format");
+  } else {
+    obj.phoneNumber = phone.value;
+  }
+}
+
+function getOccupation(){
+  obj.occupation = occupation.value;
+};
+
+function getCat() {
+  if (cat.value.length <= 0 ){
+    alert("please type in your cat's name")
+  } else {
+    obj.cat = cat.value;
+  }
+}
+
+function favGadget(){
+  if (gadget.value.length < 1){
+    alert("please input your favorite gadget")
+  } else {
+    obj.gadget = gadget.value;
+  }
+};
+function getTalent(){
+  obj.talent = talent.value;
+}
+
+function getDrink(){
+  if (drink.value.length < 1){
+    alert("please fill in the blanks")
+  } else {
+    obj.drink = drink.value;
+  }
+};
+
+function getPower(){
+  if (power.value.length < 1){
+    alert("please fill in the blanks")
+  } else {
+    obj.power = power.value;
+  }
+};
+function getWeapon(){
+  if (weapon.value.length < 1) {
+    alert("please fill in the blanks")
+  } else {
+      obj.weapon = weapon.value;
+  }
+};
+function affiliation(){
+  if(affil.value.length < 1){
+    alert("please fill in the blanks")
+  } else {
+    obj.affiliation = affil.value;
+  }
+};
+function comment(){
+  if(comments.value.length < 1){
+    alert("please fill in the blanks")
+  } else {
+    obj.comments = comments.value;
+  }
+};
 
 
 
 
+// function manualInput(x) {
+//   if (x.value.length < 0){
+//     alert("please fill in the blanks")
+//   } else {
+//     obj.x = x[value];
+//   }
+// }
 
+
+// function manualInput(x) {
+//   if (x.value.length < 0){
+//     alert("please fill in the blanks")
+//   } else {
+//     obj.x = x[value];
+//   }
+// }
+//manual inputs include drink, power, weapon
 //str.toUpperCase()
 //str.toLowerCase()
 
